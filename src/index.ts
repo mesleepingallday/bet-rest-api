884c78d7931a4e8d512c9c7a072aa238a3a155bc
Hai Nguyen <hainguyenfe@outlook.com>
2024-10-10 10:55:41 +0700

    Add multi route

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,31 @@ import { cors } from "@elysiajs/cors";
 import html from "@elysiajs/html";
 
 const port = process.env.PORT ?? 5432;
-const links = Bun.file("temp.txt");
 
-const app = new Elysia()
-  .use(cors())
-  .state("version", "1.0.0")
-  .get("/links", () => {
-    return new Response(links, {
-      headers: { "Content-Type": "text/html;charset=utf-8" },
+const app = new Elysia().use(cors()).state("version", "1.0.0").use(html());
+
+function createRoutes(app: Elysia, routeName: string, fileName: string) {
+  return app
+    .get(`/${routeName}`, () => {
+      const file = Bun.file(fileName);
+      return new Response(file, {
+        headers: { "Content-Type": "text/html;charset=utf-8" },
+      });
+    })
+    .put(`/${routeName}`, async ({ body }) => {
+      const data = typeof body === "string" ? body : JSON.stringify(body);
+      await Bun.write(fileName, data);
+      return new Response(`${routeName} saved successfully`, {
+        status: 200,
+        headers: { "Content-Type": "text/plain;charset=utf-8" },
+      });
     });
-  })
-  .use(html())
-  .put("/links", async ({ body }) => {
-    const data = typeof body === "string" ? body : JSON.stringify(body);
-    await Bun.write("temp.txt", data);
-    return new Response("Links saved successfully", {
-      status: 200,
-      headers: { "Content-Type": "text/plain;charset=utf-8" },
-    });
-  })
+}
+
+createRoutes(app, "thongtinthem", "thongtinthem.txt");
+createRoutes(app, "lienkethuuich", "lienkethuuich.txt");
+
+app
   .onError((error) => {
     console.error("Error writing file:", error);
     return new Response("Failed to save links", {
